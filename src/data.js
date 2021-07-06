664485483dfbbdc7004fae93863bcb0996a1ba49
fjg0703 <76641536+fjg0703@users.noreply.github.com>
2021-07-06 09:07:02 -0500

    frontend home page

---
+++ src/data.js
@@ -0,0 +1,64 @@
+const data = {
+  products:[
+    {
+      _id: '1',
+      name: 'Prime Porterhouse, Wet Aged 20oz',
+      price: '38',
+      description: 'Indeed the very best of both worlds, this is one hearty steak. On one side, a tender Filet Mignon; on the other side, an intensely flavorful NY Strip.  Doesn’t get much better than this! Wet aged.',
+      category: 'beef',
+      image: '/images/image_11.jpeg'
+    },
+    {
+      _id: '2',
+      name: 'Filet - Prime, Wet Aged 10oz',
+      price: '34',
+      description: 'This is the most incredibly tender, buttery texture that is center cut from a pristine tenderloin and crafted by hand. Subtly marbled that enhances an even greater depth of flavor.  If you’ve been wanting a filet that will literally melt in your mouth- you found it! ',
+      category: 'beef',
+      image: '/images/image_21.jpg'
+    },
+    {
+      _id: '3',
+      name: '14 oz Bone-In Filet Prime, Wet Aged',
+      price: '42',
+      description: '',
+      category: 'beef',
+      image: '/images/image_31.png'
+    },
+    {
+      _id: '4',
+      name: 'NY Strip - Prime, Wet Aged 16oz',
+      price: '38',
+      description: 'A steakhouse staple. Sourced from the short loin, the strip is another tender cut of beef. A cut favored for its beefy flavor, our Strip Steaks are center cut, (no tough tendons here), and offer a bit less marbling than a Ribeye. All while providing a cut that is incredibly tender & flavorful. ',
+      category: 'beef',
+      image: '/images/image_41.jpg'
+    },
+    {
+      _id: '5',
+      name: 'NY Strip - Prime, Dry Aged 16oz',
+      price: '36',
+      description: 'A cut favored for its beefy flavor, our Strip Steaks are center-cut, (no tough tendons here), and offer less marbling than a Ribeye, while providing just enough to produce a cut that is incredibly tender, rich & flavorful. Its fine marbling and 41-day aging results in a more pronounced, nuttier flavor.',
+      category: 'beef',
+      image: '/images/image_51.jpg'
+    },
+    {
+      _id: '6',
+      name: 'Boneless Ribeye - Prime, Wet Aged 16oz',
+      price: '31',
+      description: 'The Ribeye gets such high praise because it’s the most well-marbled of all the steaks and tender to boot. We handcraft this steak to showcase the brilliantly marbled center, including a nice “Rib Eye Cap” on one end which is loaded with delicious flavor. ',
+      category: 'beef',
+      image: '/images/image_61.jpg'
+    },
+    {
+      _id: '7',
+      name: '36 oz Tomahawk Prime, Wet Aged',
+      price: '64',
+      description: 'Our Happy To Meat You Prime Tomahawk Ribeye is a true stand out.   Essentially an incredibly flavorful, tender and well marbled ribeye steak, this grand dame of the beef world is specifically cut with at least five inches of rib bone left intact and can feed 2-3 people.  Perfect any time, but especially when served at your next dinner party or grilling event or even as an ever so welcomed gift of great taste!',
+      category: 'beef',
+      image: '/images/image_71.jpg'
+    },
+  ]
+
+}
+
+
+export default data;
