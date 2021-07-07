eac109d7865bb3794c0d5b561519abeb8a3fcd02
zwomack98 <83146317+zwomack98@users.noreply.github.com>
2021-07-07 14:58:55 -0500

    commit

--- server/api/products.js
+++ server/api/products.js
@@ -1,45 +1,19 @@
 const express = require('express');
-const axios = require('axios');
-const { SERVER_ADDRESS = 'http://localhost:', PORT = 5000} = process.env;
-const API_URL = process.env.API_URL || SERVER_ADDRESS + PORT;
 const { getAllProducts } = require('../db/products');
 const { getProduct } = require('../db/products');
 const { createProduct } = require('../db/products');
 const { updateProduct } = require('../db/products');
 const { destroyProduct } = require('../db/products');
 
-const { rebuildDB, testDB } = require('../db/seedData');
-const { client } =require('../db');
-const { useState } = require('react');
-
 const router = express.Router();
 
-app.get('/', (req, res) => {
-    res.send( getAllProducts)
-})
-
-app.get('/', (req, res) => {
-    res.send( getProduct )
-})
-
-app.get('/', (req, res) => {
-    res.send( createProduct)
-})
-
-app.get('/', (req, res) => {
-    res.send( updateProduct )
-})
-
-app.get('/', (req, res) => {
-    
-    res.send( destroyProduct )
+router.get('/', async (req, res) => {
+    console.log('Fetching Data')
+    let products = await getAllProducts()
+    console.log(products)
+    res.send( products )
 })
 
 
 
-// useState( () => {
-//     const fetchProducts = async () => {
-//         const res = await fetch('./db/seedData')
-//     }
-// })
-
+module.exports = router
