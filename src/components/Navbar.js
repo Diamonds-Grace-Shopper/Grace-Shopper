b48d64336039b9bcd4c5c47874fd8a8afa5e9fe3
Mariangel <mteresa.91@gmail.com>
2021-07-08 18:37:54 -0500

    Testing the cart

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -25,6 +25,7 @@ function Navbar({ user, setUser, history }) {
           <div>
             <NavLink to='/login'>Login</NavLink>
             <NavLink to='/signup'>Sign Up</NavLink>
+            <NavLink to='/src/components/Cart.js'>Cart</NavLink>
           </div>
         )}
       </nav>
