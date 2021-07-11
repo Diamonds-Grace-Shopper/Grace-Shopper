22d5c57da73c7a0ff1fa9a8b548d4c01c61fb69a
fjg0703 <76641536+fjg0703@users.noreply.github.com>
2021-07-10 21:42:49 -0500

    get data from backend

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,16 +1,43 @@
-import React from 'react';
-import data from '../data'
+import React, { useEffect, useState } from 'react';
 import Product from '../components/Product'
+import axios from 'axios';
+
+
 
 export default function HomeScreen(){
-  return (
-    <div className="row center">
-      {
-        data.products.map(product =>(
-          <Product key={product._id} product={product}></Product>
-        ))
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading ] = useState(false);
+  const [error, setError] = useState(false);
+
+
+  useEffect(() =>{
+    const fecthData = async () => {
+      try{
+        setLoading(false);
+        const { data } = await axios.get('/api/products');
+        setLoading(false)
+        setProducts(data);
+
+      }catch(error){
+        setError(error.message);
+        setLoading(false)
+
       }
+    };
+    fecthData();
+
+  },[])
+  return (
+    <div>
+
+        <div className="row center">
+      {
+        products.map(product =>(
+          <Product key={product._id} product={product}></Product>
+        ))}
   
     </div>
+    </div>
+    
   );
 }
--- src/screens/ProductScreen.js
+++ src/screens/ProductScreen.js
@@ -1,21 +1,23 @@
 import React from 'react';
-import data from '../data'
+import { Link } from 'react-router-dom';
 import Product from '../components/Product'
 
 
 
 export default function ProductScreen(props){
-  const product = data.products.find((x) => x._id === props.match.params.id);
+  const product = products.find((x) => x._id === props.match.params.id);
   if(!product){
-    return <div>Product Not Found</div>
+    return (<div>Product Not Found</div>)
   }
-  return <div>
-    <div className="row">
+  return (
+  <div>
+    <Link to="/">Back to result</Link>
+    <div className="row top">
       <div className="col-2">
-        <img src={product.image} alt={product.name}></img>
+        <img className="large" src={product.image} alt={product.name}></img>
 
       </div>
-      <div className="col-1">
+      <div className="col-1"> 
         <ul>
           <li>
             <h1>{ product.name }</h1>
@@ -47,5 +49,5 @@ export default function ProductScreen(props){
       </div>
 
     </div>
-  </div>;
+  </div>);
 }
